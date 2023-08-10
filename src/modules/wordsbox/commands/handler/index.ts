a24b56c58ca5666ea737a9121fc550e6e33e3d22
DarenGit28 <daren.shamoun28@gmail.com>
2023-08-10 19:26:53 +0330

    MODIFY: indexes

--- src/modules/wordsbox/commands/handler/index.ts
+++ src/modules/wordsbox/commands/handler/index.ts
@@ -1,8 +1,9 @@
 import { AddWordToBoxHandler } from './add-words-to-box.handler'
 import { CreateWordsBoxHandler } from './create-words-box.handler'
+import { WordsBoxHandler } from './delete-words-box.handler'
 
 export * from './create-words-box.handler'
 export * from './add-words-to-box.handler'
 
 
-export const CommandHandler =[CreateWordsBoxHandler,AddWordToBoxHandler]
+export const CommandHandler =[CreateWordsBoxHandler,AddWordToBoxHandler,WordsBoxHandler]
--- src/modules/wordsbox/commands/impl/index.ts
+++ src/modules/wordsbox/commands/impl/index.ts
@@ -1,2 +1,3 @@
 export * from './add-word-to-box.command'
 export * from './create-words-box.command'
+export * from './delete-words-box.command'

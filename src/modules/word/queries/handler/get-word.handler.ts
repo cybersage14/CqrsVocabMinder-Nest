21849182c10f6307cc8273e9360eb78f96ea175c
DarenGit28 <daren.shamoun28@gmail.com>
2023-08-12 11:38:37 +0330

    MODIFY: word handler

--- src/modules/word/queries/handler/get-word.handler.ts
+++ src/modules/word/queries/handler/get-word.handler.ts
@@ -3,6 +3,7 @@ import { WordEntity } from "../../../../entities";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
 import { GetWordQuery } from "../impl";
+import { CustomError, WORD_NOT_FOUND } from "@src/common/errors";
 
 @QueryHandler(GetWordQuery)
 export class GetWordHandler implements IQueryHandler<GetWordQuery> {
@@ -16,7 +17,9 @@ export class GetWordHandler implements IQueryHandler<GetWordQuery> {
             .andWhere('word.id = :wordId', { wordId })
             .leftJoin('word.user', 'user')
             .andWhere('user.id = :userId', { userId })
-
+        if(await queryBuilder.getOne()===null){ 
+            throw new CustomError(WORD_NOT_FOUND)
+        }
         return await queryBuilder.getOne()
     }
 

c66c839343c1ac2dcda6967ce49e10dcf02591b9
DarenGit28 <daren.shamoun28@gmail.com>
2023-09-21 06:51:00 +0000

    add: logger and interceptor

--- src/main.ts
+++ src/main.ts
@@ -4,18 +4,26 @@ import { useContainer } from 'class-validator';
 import { TrimStringsPipe } from './common/transformer/trim-strings.pipe';
 import { AppModule } from './app.module';
 import { setupSwagger } from './swagger';
+import { LoggingInterceptor } from './common/interceptors/logger.interceptor';
+import { LoggerService } from './common/logger/logger.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('/api');
+  // swagger
   setupSwagger(app);
+
   app.enableCors();
+  // pipe
   app.useGlobalPipes(new TrimStringsPipe(), new ValidationPipe());
+  // interceptor 
+  app.useGlobalInterceptors(new LoggingInterceptor(new LoggerService()));
+  // container for validator 
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
 
   const PORT = process.env.PORT || 3000;
-  
-  await app.listen(PORT,async ()=>{
+
+  await app.listen(PORT, async () => {
     console.log(`listening on port ${await app.getUrl()}`);
   });
 }

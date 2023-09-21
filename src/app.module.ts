c4170bd398fffe33d7fb7860b808a55becb968fc
DarenGit28 <daren.shamoun28@gmail.com>
2023-09-21 08:14:20 +0000

    add: rate limiter

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './modules/auth';
 import { CommonModule } from './common';
-import { ConfigModule } from './config';
+import { ConfigModule, ConfigService } from './config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './common/database/database.module';
@@ -10,6 +10,10 @@ import { CqrsModule } from '@nestjs/cqrs';
 import { WordModule } from './modules/word/word.module';
 import { WordsBoxModule } from './modules/wordsBox/wordsBox.module';
 import { BoxModule } from './modules/box/box.module';
+import { ThrottlerAsyncOptions, ThrottlerGuard, ThrottlerModule, ThrottlerOptions, seconds } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
+import { ThrottlerStorageRedisService } from 'nestjs-throttler-storage-redis';
+import { Redis } from 'ioredis';
 
 @Module({
   imports: [
@@ -21,9 +25,29 @@ import { BoxModule } from './modules/box/box.module';
     CqrsModule,
     WordModule,
     WordsBoxModule,
-    BoxModule
+    BoxModule,
+    // throttlers: [{ limit: 5, ttl: seconds(60) }],
+    // storage: new ThrottlerStorageRedisService(new Redis('redis://127.0.0.1:6379'))
+    ThrottlerModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        throttlers: [
+          {
+            limit: Number(config.get('THROTTLE_LIMIT')),
+            ttl: Number(config.get('THROTTLE_TTL')),
+          },
+        ],
+        // TODO: refactor redis config
+        storage: new ThrottlerStorageRedisService(new Redis(config.get('REDIS_URL'))),
+      }),
+    }),
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard
+    }],
 })
-export class AppModule {}
+export class AppModule { }
